a279862fbea90aba8bc276c8f5e718e3c945c700
Shashank Singh <singhshashankcic@ducic.ac.in>
2019-07-22 23:28:18 +0530

    adding day1

---
+++ Day_1/soln1.cpp
@@ -0,0 +1,21 @@
+int a = 0;
+double b = 0.0;
+string c = "";
+
+
+// Declare second integer, double, and String variables.
+cin>>a>>b;
+getline(cin>>ws,c);
+
+
+// Read and save an integer, double, and String to your variables.
+// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+
+cout<<a+i<<endl;
+// Print the sum of both integer variables on a new line.
+printf("%.1f",b+d);
+cout<<endl;
+// Print the sum of the double variables on a new line.
+cout<<s<<c;
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
