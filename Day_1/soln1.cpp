4557ab14f881434072d4cfee0b44df221ac8b7c3
Shashank Singh <singhshashankcic@ducic.ac.in>
2019-07-24 20:27:23 +0530

    adding day 4

--- Day_1/soln1.cpp
+++ Day_1/soln1.cpp
@@ -1,21 +1,33 @@
-int a = 0;
-double b = 0.0;
-string c = "";
+#include <iostream>
+#include <iomanip>
+#include <limits>
 
+using namespace std;
 
-// Declare second integer, double, and String variables.
-cin>>a>>b;
-getline(cin>>ws,c);
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int a = 0;
+    double b = 0.0;
+    string c = "";
 
+    
+    // Declare second integer, double, and String variables.
+    cin>>a>>b;
+    getline(cin>>ws,c);
+    
 
-// Read and save an integer, double, and String to your variables.
-// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    cout<<a+i<<endl;
+    // Print the sum of both integer variables on a new line.
+    printf("%.1f",b+d);
+    cout<<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<s<<c;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
 
-cout<<a+i<<endl;
-// Print the sum of both integer variables on a new line.
-printf("%.1f",b+d);
-cout<<endl;
-// Print the sum of the double variables on a new line.
-cout<<s<<c;
-// Concatenate and print the String variables on a new line
-// The 's' variable above should be printed first.
+    return 0;
